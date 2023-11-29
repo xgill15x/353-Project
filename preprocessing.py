a3beab027b90cc4bf6a1e78963b5f0499c90da3e
xgill15x <jason_gill15@hotmail.com>
2023-11-28 20:55:31 -0800

    init preprocessing file

--- preprocessing.py
+++ preprocessing.py
@@ -6,18 +6,14 @@ def fill_na_with_median(df, column_name):
 
 df = pd.read_csv('data.csv')
 
-# df2 = pd.read_csv('preprocessed_data.csv')
-
 df = df[~(df['Winner'] == 'Draw')]
-# df = df[['R_fighter', 'R_Stance', 'B_fighter', 'B_Stance', 'Winner']]
-print(df.reset_index(inplace=True))
+df.reset_index(inplace=True)
 
 numerical_columns = df.select_dtypes(include=['number'])
-
 for column in numerical_columns[1:]:    # skips index
     fill_na_with_median(df, column)
 
-
-# # Write the DataFrame to a CSV file
+# Write the DataFrame to a CSV file
 df.to_csv("recreate.csv", index=False)
+
 
