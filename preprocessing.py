a80f9b43394e8492143bc6d6152dd419a15d2817
xgill15x <jason_gill15@hotmail.com>
2023-11-28 20:58:44 -0800

    added main wrapper for preprocessing file

--- preprocessing.py
+++ preprocessing.py
@@ -4,16 +4,21 @@ def fill_na_with_median(df, column_name):
     median = df[column_name].median()
     df[column_name] = df[column_name].fillna(value=median)
 
-df = pd.read_csv('data.csv')
+def main():
+    df = pd.read_csv('data.csv')
 
-df = df[~(df['Winner'] == 'Draw')]
-df.reset_index(inplace=True)
+    df = df[~(df['Winner'] == 'Draw')]
+    df.reset_index(inplace=True)
 
-numerical_columns = df.select_dtypes(include=['number'])
-for column in numerical_columns[1:]:    # skips index
-    fill_na_with_median(df, column)
+    numerical_columns = df.select_dtypes(include=['number'])
+    for column in numerical_columns[1:]:    # skips index
+        fill_na_with_median(df, column)
 
-# Write the DataFrame to a CSV file
-df.to_csv("recreate.csv", index=False)
+    # Write the DataFrame to a CSV file
+    df.to_csv("recreate.csv", index=False)
+
+
+if __name__=='__main__':
+    main()
 
 
